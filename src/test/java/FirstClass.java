af6324a56a4d0f39aa2cf81bcbcd0a287dcc7e69
AleshkovAA <arsenii-2020@mail.ru>
2022-04-15 18:11:21 +0700

    init

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,52 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FirstTest {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get("https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d")
+
+        driver.get("https://sandbox.cardpay.com/MI/payment.html?uuid=DGe2h7dE5GBab7a340hGdggB");
+        driver.findElement(By.id("input-card-number")).clear();
+        driver.findElement(By.id("input-card-number")).sendKeys("4000 0000 0000 0002");
+        driver.findElement(By.id("input-card-holder")).click();
+        driver.findElement(By.id("input-card-holder")).clear();
+        driver.findElement(By.id("input-card-holder")).sendKeys("ARSENII ALESHKOV");
+        driver.findElement(By.id("card-expires-month")).click();
+        new Select(driver.findElement(By.id("card-expires-month"))).selectByVisibleText("10");
+        driver.findElement(By.id("card-expires-year")).click();
+        new Select(driver.findElement(By.id("card-expires-year"))).selectByVisibleText("2035");
+        driver.findElement(By.id("input-card-cvc")).click();
+        driver.findElement(By.id("input-card-cvc")).clear();
+        driver.findElement(By.id("input-card-cvc")).sendKeys("000");
+        driver.findElement(By.id("action-submit")).click();
+        driver.get("https://sandbox.cardpay.com/acs-emulator/dummy?p1=v1&p2=v2");
+        driver.findElement(By.id("success")).click();
+        driver.get("https://sandbox.cardpay.com/MI/cardpayment2.html?bank_id=sandbox");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+}
